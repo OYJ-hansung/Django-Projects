9504b7debc0bb1b266d768d85649fe8d696bfece
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-04-14 17:14:00 +0900

    updates

--- sessionAPI/sessionApp/views.py
+++ sessionAPI/sessionApp/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .forms import ItemForm
+from django.http import HttpResponse
 
 # Create your views here.
+
+# No Cookies but with sessions.
+
+def pageCount(request):
+    count = request.session.get('count', 0) # if not exist default is 0
+    count += 1
+    request.session['count'] = count
+    return render(request, 'sessionApp/count.html', {'count': count})
+
+
+def index(request):
+    request.session.set_expiry(100)
+    del request.session['count']
+    return render(request, 'sessionApp/index.html')
+
+# with sessions / not cookies
+def addItem(request):
+    form = ItemForm()
+    if request.method == 'POST':
+        name = request.POST['name']
+        quantity = request.POST['quantity']
+        request.session[name] = quantity
+    return render(request, 'sessionApp/addItem.html', {'form' : form})
+
+
+def displayCart(request):
+    return render(request, 'sessionApp/displayItems.html')
+
