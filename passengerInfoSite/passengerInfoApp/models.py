f04fa58e4959cb51f87f47010ecad4d2343cd791
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-04-14 17:18:45 +0900

    updates

--- passengerInfoSite/passengerInfoApp/models.py
+++ passengerInfoSite/passengerInfoApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+#test
 class Passenger(models.Model):
     firstName = models.CharField(max_length=15)
     lastName = models.CharField(max_length=30)
     email = models.CharField(max_length=35)
     rewardPoints = models.FloatField()
+    
